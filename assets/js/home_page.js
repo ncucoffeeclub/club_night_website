8ddabef953c26c4b596b8e7740be88bb0f47f29e
ncucoffeeclub <ncucoffeeclub@gmail.com>
2016-07-04 19:34:24 +0800

    add time schema

---
+++ assets/js/home_page.js
@@ -0,0 +1,39 @@
+
+function checkTime(i)
+{
+if (i<10) 
+  {i="0" + i}
+  return i;
+}
+var spantime;
+var startDate;
+function startTime(){
+    spantime --;
+    var d = Math.floor(spantime / (24 * 3600));
+    var h = Math.floor((spantime % (24*3600))/3600);
+    var m = Math.floor((spantime % 3600)/(60));
+    var s = Math.floor(spantime%60);
+    // add a zero in front of numbers<10
+    m=checkTime(m);
+    s=checkTime(s);
+    if(spantime>0){
+        $('#day').text(d);
+        $("#hour").text(h+(d*24));
+        $("#min").text(m);
+        $("#sec").text(s);
+    }else{ // 避免倒數變成負的
+        $('#day').text(0);
+        $("#hour").text(0);
+        $("#min").text(0);
+        $("#sec").text(0);
+    }
+}
+
+function set_timer_counter(){
+    startDate = new Date();
+    var endDate = new Date('2016/7/4 18:00');
+    spantime = (endDate - startDate)/1000;
+    $(document).ready(function () {
+        setTimeout('startTime()',500)
+    });
+}
