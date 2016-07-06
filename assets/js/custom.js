05814a61360c6dcad97a27bcf204df3d75aea3b0
ncucoffeeclub <ncucoffeeclub@gmail.com>
2016-07-07 00:44:48 +0800

    first page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -45,6 +45,11 @@
 			offset: 70
 		})
 
+		$('.nav a').on('click', function(){
+		    $('.btn-navbar').click(); //bootstrap 2.x
+		    $('.navbar-toggle').click() //bootstrap 3.x by Richard
+		});
+
         
         /* ---------------------------------------------- /*
 		 * Skills
