d213edec180ba2a9f7b019673462a12d8e30b395
ncucoffeeclub <ncucoffeeclub@gmail.com>
2016-07-10 05:40:20 +0800

    Version 1.1

--- assets/js/home_page.js
+++ assets/js/home_page.js
@@ -235,6 +235,6 @@ function change_coffee_corner_text(item){
     iBase.Id('coffee_corner_subtitle').innerHTML = coffee_corner_array[item].subtitle;
     iBase.Id('coffee_corner_context').innerHTML = coffee_corner_array[item].context;
     iBase.Id('coffee_corner_img').src = coffee_corner_array[item].photo;
-    $('#coffee_corner_facebook-like').data('href', coffee_corner_array[item].url);
+    $(".fb-like").attr('data-href', coffee_corner_array[item].url);
     fadeIn(iBase.Id('coffee_corner_container'));
 }
